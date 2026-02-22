8c6d76aa278e6ede9d14062d5e4ea8528c682489
Nick Switzer <nicholas.switzer@gmail.com>
2026-02-22 20:58:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4236902803-c4aa9",
+  "appId": "1:170458557546:web:947afaf1c4c2ddd9fe2e13",
+  "apiKey": "AIzaSyCoQZbgbIU7v_76jh38CxwDIqThX6_7c3U",
+  "authDomain": "studio-4236902803-c4aa9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "170458557546"
+};
